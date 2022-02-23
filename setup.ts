fa825be53d39eca9c3f3ad02a2ac83e1152ee656
Geri <luga.geri@gmail.com>
2022-02-23 21:29:57 +0100

    Made emails unique

--- setup.ts
+++ setup.ts
@@ -26,7 +26,7 @@ DROP TABLE IF EXISTS subreddits ;
 CREATE TABLE IF NOT EXISTS "users" (
     "id" INTEGER PRIMARY KEY,
     "username" TEXT  NOT NULL,
-    "email" TEXT NOT NULL,
+    "email" TEXT NOT NULL UNIQUE,
     "password" TEXT NOT NULL
   );
 
