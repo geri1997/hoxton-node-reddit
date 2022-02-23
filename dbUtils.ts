f59ca097cdb4ebeca36ec2e93270cc0d684e018d
Geri <luga.geri@gmail.com>
2022-02-23 22:53:58 +0100

    Added some db functions and types

--- dbUtils.ts
+++ dbUtils.ts
@@ -1,4 +1,5 @@
 import Database from 'better-sqlite3';
+import { IPost, ISubreddit, IUser } from './types';
 
 const db = new Database('./data.db', {
    verbose: console.log,
@@ -20,7 +21,24 @@ export const createUser = (username: string, email: string, password: string) =>
       .prepare(`INSERT INTO users (username,email,password) VALUES (?, ?, ?)`)
       .run(username, email, password);
 
-export const getUserBySpecificX = (column: string, value: string | number) =>
+export const getUserBySpecificX = (column: string, value: string): IUser =>
    db
       .prepare(`SELECT * FROM users WHERE UPPER(${column})= UPPER(?);`)
+      .get(value);
+
+export const getAllPostsOrderedByUpvotes = (): IPost[] =>
+   db
+      .prepare(
+         `SELECT *
+      FROM posts
+      ORDER BY upvotes DESC;`
+      )
+      .all();
+
+export const getSubredditBySpecificX = (
+   column: string,
+   value: string
+): ISubreddit =>
+   db
+      .prepare(`SELECT * FROM subreddits WHERE UPPER(${column})= UPPER(?);`)
       .get(value);
