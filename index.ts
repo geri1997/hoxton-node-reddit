3dea3b666dfef844f006a2de2f5e14c988ee4f8f
Geri <luga.geri@gmail.com>
2022-02-23 21:45:09 +0100

    Created the sign up endpoint

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { getUserBySpecificX } from './dbUtils';
+import { createUser, getUserBySpecificX } from './dbUtils';
 
 const PORT = 3009;
 const app = express();
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/log-in', (req, res) => {
-const {email,password} = req.body
+   const { email, password } = req.body;
    const user = getUserBySpecificX('email', email);
 
    if (!user)
@@ -16,7 +16,24 @@ const {email,password} = req.body
    if (user && user.password !== password)
       return res.send({ error: `Wrong password` });
 
-      res.send(user)
+   res.send(user);
+});
+
+app.post('/sign-up', (req, res) => {
+   const { email, username, password } = req.body;
+   const existingUser = getUserBySpecificX('email', email);
+
+   if (existingUser)
+      return res
+         .status(409)
+         .send({ error: `A user with this email already exists.` });
+   createUser(username, email, password);
+   if (!email || !username || !password)
+      return res
+         .status(400)
+         .send({ error: `Missing needed properties to create a user` });
+
+   res.status(201).send(getUserBySpecificX('email', email));
 });
 
 app.listen(PORT, () => {
